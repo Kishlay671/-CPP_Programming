2f89440cc19cf412db045077fd3121a529ea38f8
Kishlay671 <155213892+Kishlay671@users.noreply.github.com>
2025-01-17 09:02:40 +0530

    basics

---
+++ Basics/input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int k;
+    cout<<"Enter even:"<<endl<<"Enter odd:";
+    cin>>n>>k;
+    cout<<n+k;
+}
---
+++ Basics/nextline.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello PW";
+    cout<<"Hello CW"<<endl;
+    //FOR NEXT LINE WE USE endl OR \n
+    cout<<"Hello PW"<<endl;
+    cout<<"Hello CW"<<endl;
+    //OR
+    cout<<"Hello PW\nHello CW"<<endl;
+    //OR
+    cout<<"Hello PW"<<endl<<"Hello CW";
+    return 0;
+}
